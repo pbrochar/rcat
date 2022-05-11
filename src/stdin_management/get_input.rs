db8a75cfe82926f927062a08f33fc6abbdbb1e80
Paul <paul@debian.paul>
2022-05-11 22:21:46 +0200

    final commit

--- src/stdin_management/get_input.rs
+++ src/stdin_management/get_input.rs
@@ -1,6 +1,7 @@
 use crate::args_management::command::Commande;
 use crate::print_data::print_file::print_content;
 use std::{io, io::prelude::*};
+use crate::PROG_NAME;
 
 pub fn get_stdin_input(command: &Commande, index: Option<&mut i32>) {
 	let line_index;
@@ -14,7 +15,7 @@ pub fn get_stdin_input(command: &Commande, index: Option<&mut i32>) {
 		match line {
 			Ok(line) => print_content(command, &line.trim_end().to_string(), line_index),
 			Err(_error) => {
-				println!("rcat: an error occured");
+				println!("{}: an error occured", PROG_NAME);
 				break;
 			}
 		}
